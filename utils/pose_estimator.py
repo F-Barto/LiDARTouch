3968f764a579c208ca9f91d90a4aa4cf74689844
F-Barto <fbartoc@hotmail.fr>
2021-03-06 16:43:46 +0100

    correct pnp method according to nb of valid points (P3P, EPnP)

--- utils/pose_estimator.py
+++ utils/pose_estimator.py
@@ -1,3 +1,9 @@
+'''
+Code adapted from
+ https://github.com/fangchangma/self-supervised-depth-completion/blob/master/dataloaders/pose_estimator.py
+'''
+
+
 import cv2
 import numpy as np
 
@@ -72,8 +78,7 @@ def get_pose_pnp(rgb_curr, rgb_near, depth_curr, K):
 
     # extract 3d pts
     pts3d_curr = []
-    pts2d_near_filtered = [
-    ]  # keep only feature points with depth in the current frame
+    pts2d_near_filtered = []  # keep only feature points with depth in the current frame
     for i, pt2d in enumerate(pts2d_curr):
         # print(pt2d)
         u, v = pt2d[0], pt2d[1]
@@ -83,24 +88,28 @@ def get_pose_pnp(rgb_curr, rgb_near, depth_curr, K):
             pts3d_curr.append(xyz_curr)
             pts2d_near_filtered.append(pts2d_near[i])
 
-    pts3d_curr = np.expand_dims(np.array(pts3d_curr).astype(np.float32), axis=1)
-    pts2d_near_filtered = np.expand_dims(
-        np.array(pts2d_near_filtered).astype(np.float32), axis=1)
+    if len(pts3d_curr) >= 4 and len(pts2d_near_filtered) >= 4:
 
-    same_length = pts3d_curr.shape[0] == pts2d_near_filtered.shape[0]
-    # the minimal number of points accepted by solvePnP is 4:
-    required_count = pts3d_curr.shape[0] >= 4 and pts2d_near_filtered.shape[0] >= 4
+        flag = cv2.SOLVEPNP_EPNP
+        if len(pts3d_curr) == 4 and len(pts2d_near_filtered) == 4:
+            flag = cv2.SOLVEPNP_P3P
 
-    if same_length and required_count:
-        # ransac
+        pts3d_curr = np.expand_dims(np.array(pts3d_curr).astype(np.float32), axis=1)
+        pts2d_near_filtered = np.expand_dims(np.array(pts2d_near_filtered).astype(np.float32), axis=1)
+
+        # https://docs.opencv.org/3.4/d9/d0c/group__calib3d.html
+        # The default method used to estimate the camera pose for the Minimal Sample Sets step is SOLVEPNP_EPNP.
+        # Exceptions are:
+        # if you choose SOLVEPNP_P3P or SOLVEPNP_AP3P, these methods will be used.
+        # if the number of input points is equal to 4, SOLVEPNP_P3P is used
         ret = cv2.solvePnPRansac(pts3d_curr,
                                  pts2d_near_filtered,
                                  K[:3,:3],
                                  distCoeffs=None,
-                                 flags=cv2.SOLVEPNP_P3P)
+                                 flags=flag)
         success = ret[0]
         rotation_vector = ret[1]
         translation_vector = ret[2]
         return (success, rotation_vector, translation_vector)
     else:
-        return (0, None, None)
+        return (False, None, None)
