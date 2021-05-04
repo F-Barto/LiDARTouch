16562acfa74ae73da7bb6bf2207d4f5af1c352ed
F-Barto <fbartoc@hotmail.fr>
2021-05-04 21:27:57 +0200

    correct guidenet for multi-gpu

--- networks/nets/depth_nets/guidenet.py
+++ networks/nets/depth_nets/guidenet.py
@@ -59,19 +59,21 @@ class Conv2dLocal(nn.Module):
 class Basic2d(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer=None, kernel_size=3, padding=1):
         super().__init__()
-        if norm_layer:
-            conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-                             stride=1, padding=padding, bias=False)
-        else:
-            conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-                             stride=1, padding=padding, bias=True)
-        self.conv = nn.Sequential(conv, )
-        if norm_layer:
-            self.conv.add_module('bn', norm_layer(out_channels))
-        self.conv.add_module('relu', nn.ReLU(inplace=True))
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                             stride=1, padding=padding, bias=(norm_layer is None))
+
+        self.norm_layer = norm_layer
+        if norm_layer is not None:
+            self.norm = norm_layer(out_channels)
+
+        self.act = nn.ReLU(inplace=True)
 
     def forward(self, x):
         out = self.conv(x)
+        if self.norm_layer is not None:
+            out = self.norm(out)
+        out = self.act(out)
+
         return out
 
 
@@ -246,6 +248,7 @@ class GuideNet(nn.Module):
         return True
 
     def forward(self, img, lidar):
+
         c0_img = self.conv_img(img)
         c1_img = self.layer1_img(c0_img)
         c2_img = self.layer2_img(c1_img)
@@ -328,4 +331,4 @@ def GN():
 
 
 def GNS(weight_ks=1):
-    return GuideNet(norm_layer=encoding.nn.SyncBatchNorm, guide=Guide, weight_ks=weight_ks)
+    return GuideNet(guide=Guide, weight_ks=weight_ks)
